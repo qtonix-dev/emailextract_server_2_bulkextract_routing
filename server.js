0316d174ed2b702ed14cfe4d43bdc6dde5c837d3
Biswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-11-16 10:41:39 +0530

    '16NOV2021'

--- server.js
+++ server.js
@@ -51,8 +51,142 @@ app.use(morgan('dev'));
 app.use(cors())
 
 app.get('/',(req,res)=>{
-    res.send('<h1>SERVER_2_BULK_EXTRACT</h1>');
+    // res.send('<h1>SERVER_2_BULK_EXTRACT</h1>');
+
+
+
+    const puppeteer = require('puppeteer');
+    const cheerio = require('cheerio');
+
+    async function main() {
+      const browser = await puppeteer.launch({headless: false});
+      const page = await browser.newPage();
+      await page.setViewport({width: 1200, height: 720});
+      await page.goto('http://localhost/project/wordpress/wp-login.php', { waitUntil: 'networkidle0' }); // wait until page load
+      await page.type('#user_login', 'b21341995returns');
+      await page.type('#user_pass', 'gurubaba@123');
+      // click and wait for navigation
+      await Promise.all([
+        page.click('#wp-submit'),
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        // page.goto('http://localhost/project/wordpress/wp-admin/plugins.php')
+      ]);
+
+      await page.goto('http://localhost/project/wordpress/wp-admin/plugins.php', { waitUntil: 'networkidle0' })
+      const content = await page.content();
+      const $ = cheerio.load(content);
+
+
+
+      // console.log($.html())
+
+
+      // $('tbody#the-list > tr > td > strong').toArray().map(item => {
+      //   console.log($(item).text());
+      //   // console.log(item)
+      // });
+
+      var totaldata=[]
+
+      // $('tbody#the-list > tr > td > div > span a').toArray().map(item => {
+      //   console.log($(item).attr('href'));
+      //   // console.log(item)
+      // });
+
+
+      // $('tbody#the-list > tr > td > div > span a').toArray().map(item => {
+      //   console.log($(item).attr('href'));
+      //   // console.log(item)
+      // });
+
+
+      $('tbody#the-list > tr').toArray().map(item => {
+        // console.log($(item).find('td.plugin-title strong').text());
+
+        var datas={
+          pluginname:$(item).find('td.plugin-title.column-primary strong').text(),
+          pluginversion:$(item).find('td.column-description.desc div.inactive.second.plugin-version-author-uri').text(),
+
+          activelink:$(item).find('td.plugin-title.column-primary div.row-actions span.activate a').attr('href'),
+          deletelink:$(item).find('td.plugin-title.column-primary div.row-actions span.delete a').attr('href'),
+
+        }
+        totaldata.push(datas)
+        // console.log(item)
+      });
+
+
+
+      res.json({
+        response:true,
+        data:totaldata
+      })
+      console.log(totaldata[0].activelink)
+
+      // await page.goto('http://localhost/project/wordpress/wp-admin/'+totaldata[0].activelink, { waitUntil: 'networkidle0' })
+
+
+      // await browser.close();
+    }
+
+    main();
 });
+
+
+
+
+
+
+
+app.get('/themename',(req,res)=>{
+    // res.send('<h1>SERVER_2_BULK_EXTRACT</h1>');
+
+
+
+    const puppeteer = require('puppeteer');
+    const cheerio = require('cheerio');
+
+    async function main() {
+      const browser = await puppeteer.launch({headless: false});
+      const page = await browser.newPage();
+      await page.setViewport({width: 1200, height: 720});
+      await page.goto('http://localhost/project/wordpress/wp-login.php', { waitUntil: 'networkidle0' }); // wait until page load
+      await page.type('#user_login', 'b21341995returns');
+      await page.type('#user_pass', 'gurubaba@123');
+      // click and wait for navigation
+      await Promise.all([
+        page.click('#wp-submit'),
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        // page.goto('http://localhost/project/wordpress/wp-admin/plugins.php')
+      ]);
+
+      await page.goto('http://localhost/project/wordpress/wp-admin/themes.php', { waitUntil: 'networkidle0' })
+      const content1 = await page.content();
+      const $ = cheerio.load(content1);
+
+
+      // var theme = $('span:contains("Active:")').text()
+      var theme = $('div.theme.active div.theme-id-container h2').text()
+
+
+
+
+
+      res.json({
+        response:true,
+        theme:theme
+      })
+    }
+
+    main();
+});
+
+
+
+
+
+
+
 
 
 const PORT = process.env.PORT || 5002;
